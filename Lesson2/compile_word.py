2d2f329b9873acdd1783d3bf6836b573ee03b28f
Luqi Pan <luqi.pan@gmail.com>
2014-04-02 22:59:36 -0400

    Finished lesson 2 lecture

---
+++ Lesson2/compile_word.py
@@ -0,0 +1,18 @@
+# Uer Instructions
+#
+# Write a function, compile_word(word), that compiles a word
+# of UPPERCASE letters as numeric digits. For example:
+# compile_word('YOU') => '(1*U + 10*O +100*Y)' 
+# Non-uppercase words should remain unchaged.
+
+def compile_word(word):
+    """Compile a word of uppercase letters as numeric digits.
+    E.g., compile_word('YOU') => '(1*U+10*O+100*Y)'
+    Non-uppercase words unchanged: compile_word('+') => '+'"""
+    # Your code here.
+    if not re.search("[A-Z]", word):
+        return word
+    result = []
+    for i in range(len(word)):
+        result.append(str(10**(len(word)-i-1)) + '*' + word[i])
+    return '(' + '+'.join(result) + ')'
